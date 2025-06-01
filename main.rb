da72098b64f7b77ce23fc2ec1978ef3be3dc1330
Andy Waite <13400+andyw8@users.noreply.github.com>
2025-06-01 15:59:09 -0400

    Add system prompt example

--- main.rb
+++ main.rb
@@ -22,6 +22,19 @@ class Main
     end
   end
 
+  # https://building-with-llms-pycon-2025.readthedocs.io/en/latest/prompting-python.html#using-system-prompts
+  def system_prompt_example
+    chat_completion = openai.chat.completions.create(
+      messages: [
+          {role: "system", content: "Translate this to spanish"},
+          {role: "user", content: "What is the best thing about a pelican?"}
+        ],
+      model: :"gpt-4.1"
+    )
+
+    puts(chat_completion)
+  end
+
   private
 
   def openai
@@ -31,5 +44,6 @@ end
 
 main = Main.new
 
-main.chat_completion_example
-main.stream_example
+# main.chat_completion_example
+# main.stream_example
+main.system_prompt_example
