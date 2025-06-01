8eaa63674c93ddcff36dbebbdfd4551fa54f763b
Andy Waite <13400+andyw8@users.noreply.github.com>
2025-06-01 15:25:14 -0400

    Add streaming example, and restructure

--- main.rb
+++ main.rb
@@ -2,11 +2,34 @@ require "bundler/setup"
 require "openai"
 require "dotenv/load"
 
-openai = OpenAI::Client.new
+class Main
+  def chat_completion_example
+    chat_completion = openai.chat.completions.create(
+      messages: [{role: "user", content: "Say this is a test"}],
+      model: :"gpt-4.1"
+    )
 
-chat_completion = openai.chat.completions.create(
-  messages: [{role: "user", content: "Say this is a test"}],
-  model: :"gpt-4.1"
-)
+    puts(chat_completion)
+  end
 
-puts(chat_completion)
+  def stream_example
+    stream = openai.chat.completions.stream_raw(
+      messages: [{role: "user", content: "Say this is a test"}],
+      model: :"gpt-4.1"
+    )
+    stream.each do |completion|
+      print completion.choices.first.delta.content
+    end
+  end
+
+  private
+
+  def openai
+    @openai ||= OpenAI::Client.new
+  end
+end
+
+main = Main.new
+
+main.chat_completion_example
+main.stream_example
